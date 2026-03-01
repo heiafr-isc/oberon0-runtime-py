dcb12f9fbb06b5ba6c936a1c5348558f8a535570
Jacques Supcik <jacques.supcik@hefr.ch>
2026-03-01 23:12:13 +0100

    Add return type annotation to test_cli function

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -10,7 +10,7 @@ from typer.testing import CliRunner
 from oberon0_runtime import app
 
 
-def test_cli():
+def test_cli() -> None:
     subprocess.run(["wat2wasm", "-o", "tests/add.wasm", "examples/add.wat"], check=True)
     runner = CliRunner()
     result = runner.invoke(app, ["run", "tests/add.wasm", "add", "29", "13"])
