a39a6fb00f704fc51789a50ead2468861f481dd9
Jacques Supcik <jacques.supcik@hefr.ch>
2026-03-13 11:10:50 +0100

    Bump version: 0.2.0 → 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, str(Path(__file__).parent / "../src"))
 project = "Oberon-0 Runtime"
 copyright = "2026, Jacques Supcik"
 author = "Jacques Supcik"
-release = "0.2.0"
+release = "0.2.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
